f1be770aeb5fb9fe703c815e2a6e75880808993c
Ingrid <ingridsu@yahoo.com>
2019-11-26 11:00:54 -0800

    didn't check on each element return true when first element can be found in magzine

--- harmlessRandsomeNote.js
+++ harmlessRandsomeNote.js
@@ -1,3 +1,38 @@
-function harmlessRansomeNote( noteText, magazineText ) {
+// Find out what noteText need to be find
+    // split text into array and count how many times each appear and saved as Object
+// Do the same thing with magazineText and compare if noteText appears within magazineTextObj and appearance times is equal or less
+
+function harmlessRansomNote( noteText, magazineText ) {
+    var noteTextArray = noteText.split(' ');
+    var magazineTextArray = magazineText.split(' ');
+    var noteTextObj = {};
+    var magazineTextObj = {};
+
+    for(let i = 0; i < noteTextArray.length; i ++) {
+        var char = noteTextArray[i];
+        if(!noteTextObj[char]){
+            noteTextObj[char] = 0;
+        }
+
+        noteTextObj[char] ++
+    }
+
+    for(let i = 0; i < magazineTextArray.length; i ++) {
+        var char = magazineTextArray[i];
+        if(!magazineTextArray[char]){
+            magazineTextObj[char] = 0;
+        }
+
+        magazineTextObj[char] ++
+    }
     
+    for(let keyNote in noteTextObj){
+        for(let keyMag in magazineTextObj) {
+            if(keyNote === keyMag && noteTextObj[keyNote] <= magazineTextObj[keyMag]){
+                return true
+            }
+        }
+    }
+
+    return false
 }
